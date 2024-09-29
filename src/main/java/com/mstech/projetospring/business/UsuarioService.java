0b8757f2635eb21a936a29649bea94686ded560e
Ulisses Moraes Dos Reis Souza <ulissesmoraes74@gmail.com>
2024-09-29 18:46:34 -0400

    feat:Implementação da Lista de Usuários

--- src/main/java/com/mstech/projetospring/business/UsuarioService.java
+++ src/main/java/com/mstech/projetospring/business/UsuarioService.java
@@ -6,6 +6,9 @@ import com.mstech.projetospring.infrastructure.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class UsuarioService {
@@ -34,5 +37,13 @@ public class UsuarioService {
 
     public boolean verificaEmailExistente(String email) {
         return usuarioRepository.existsByEmail(email);
+    }
+
+    public List<Usuario> listaUsuario() {
+        return usuarioRepository.findAll();
+    }
+
+    public Optional<Usuario> buscarPorId(Long id) {
+        return usuarioRepository.findById(id);
     }
 }
--- src/main/java/com/mstech/projetospring/controller/UsuarioController.java
+++ src/main/java/com/mstech/projetospring/controller/UsuarioController.java
@@ -2,11 +2,12 @@ package com.mstech.projetospring.controller;
 import com.mstech.projetospring.business.UsuarioService;
 import com.mstech.projetospring.infrastructure.entity.Usuario;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuario")
@@ -19,4 +20,19 @@ public class UsuarioController {
     public ResponseEntity<Usuario> salvarUsuario(@RequestBody Usuario usuario) {
         return ResponseEntity.ok(usuarioService.salvarUsuario(usuario));
     }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Usuario> listaUsuario() {
+        return usuarioService.listaUsuario();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Usuario buscarUsuarioPorId(@PathVariable("id") Long id) {
+        return usuarioService.buscarPorId(id)
+                .orElseThrow(() -> new
+                        ResponseStatusException(HttpStatus.NOT_FOUND, "Usuario Não Cadastrado."));
+    }
+
 }
